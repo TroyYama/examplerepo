98a0f48872a3c84c72b81af1cfe20c147fe7c408
Troy Yamashiro <troyyamashiro@Troys-MacBook-Pro.local>
2021-09-03 12:39:44 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace H3llo_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome MIS students");
+            Console.WriteLine("hope you enjoy the course");
+        }
+    }
+}
+ 
